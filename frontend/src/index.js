6e8ed205b096ceeb91c6f6de0b8a9ae0ed5f1be4
sachit-lele <sachit.lele.btech2021@sitpune.edu.in>
2024-11-16 01:15:32 +0530

    push changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import './index.css'; // You can include a global stylesheet here
 import App from './App'; // Import your main App component
 
-// Render the App component inside the root element
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root') // This targets the div with id="root" in your public/index.html
+  </React.StrictMode>
 );
+
+reportWebVitals();
